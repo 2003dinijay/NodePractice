8a5d6b20638246b5a8ea987fac839d584e6baa2e
Dinithi Nanayakkara <dinijayo@gmail.com>
2025-12-10 22:51:41 +0530

    feat: add file reading and writing examples with synchronous methods

--- practice-4/app.js
+++ practice-4/app.js
@@ -1,2 +1,16 @@
-const {readFileSync}= require('fs');  //synchronous way to read files
-const {writeFileSync}= require('fs'); //synchronous way to write files
+// const {readFileSync}= require('fs');  //synchronous way to read files
+// const {writeFileSync}= require('fs'); //synchronous way to write files
+
+const {readFileSync, writeFileSync}= require('fs'); 
+const first = readFileSync('./content/first.txt','utf8'); //specify encoding to get string otherwise it returns buffer
+const second = readFileSync('./content/second.txt','utf8');
+console.log(first, second);
+
+writeFileSync(
+    './content/result-sync.txt', 
+    `Here is the result : ${first}, ${second}`, 
+    {flag: 'a'});
+    
+//flag a is for append if file already exists
+
+//if flag is "a", it will append the content to the existing file instead of overwriting it.
